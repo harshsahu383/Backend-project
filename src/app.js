de8ae0e35b4545a89766d2c77fc54f49fc88dc35
harshsahu383 <harshsahu1917@gmail.com>
2025-08-21 19:21:01 +0530

    Somr Basic Configurations

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+// require configurations
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static("public"));
+//cors configurations
+app.use(cors({
+  origin: process.env.CORS_ORIGIN?.split(",") || "http:localhost:1800",
+  credentials : true,
+  methods : ["GET" , "POST" , "PATCH" , "PUT" , "DELETE", "OPTIONS"],
+  allowedHeaders: ["Authorization" , "Content-Type"]
+}))
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import dotenv from "dotenv";
+import dotenv  from "dotenv";
+dotenv.config({
+    path : "./env/"
+})
+import app from "./app.js"
 
-console.log("hello");
+let port = process.env.PORT || 1800;
+app.listen(port, (req, res) => {
+  console.log("server is been started");
+});
