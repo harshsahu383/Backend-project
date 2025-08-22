dd2aa3f2a8d1ecdf4687f4033eb15bbafbe74234
harshsahu383 <harshsahu1917@gmail.com>
2025-08-22 23:17:57 +0530

    First Rest Created : /healthCheck

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,16 +1,9 @@
 import {Apiresponse} from "../utils/api-response.js";
-const healthCheck  = (req , res) => {
-    try {
-        console.log("healthcheck called");
-        
-        res.status(200).json(
-            new Apiresponse(
-                200,
-                {message : "Server is running"}
-            )
-        )
-    } catch (error) {
-        
-    }
-}
+import {asyncHandler} from "../utils/async-handler.js"
+const healthCheck = asyncHandler(async (req,res) => {
+    res.status(200)
+    .json( new Apiresponse(200,{message:"Server is running"}));
+});
+
+
 export { healthCheck } ;
